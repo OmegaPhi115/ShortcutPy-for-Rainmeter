753a269d812c76480471990a2f20a5f03a0aaab2
OmegaPhi115 <45884990+OmegaPhi115@users.noreply.github.com>
2019-01-06 18:00:21 +0100

    Update ShortcutPy for Rainmeter.py

--- ShortcutPy for Rainmeter.py
+++ ShortcutPy for Rainmeter.py
@@ -1,4 +1,25 @@
+#startup
 import os, sys, winsound
-name_of_shortcut = input("Name of the Shortcut")
-image_name = "#@#Images\\" + input("Image Name ?")
-path_to_executable = input("Path to the executable")
+
+def path_exist(path):
+    if os.path.exists(path):
+        pass
+    else:
+        raise OSError
+# function that create the new shortcurt
+def new_shortcut():
+    name_of_shortcut = input("Name of the Shortcut")
+    image_name = "#@#Images\\" + input("Image Name ?")
+    path_to_executable = input("Path to the executable")
+
+
+
+
+#mainloop
+while 1:
+    print("Create a new shortcut ? Y/N")
+    a = input("")
+    if a == "Y" or a == "y":
+        new_shortcut()
+    else:
+        break
