f86c4a93c2b0c1bbcd1ccbdaa9420ac6f598ef6c
François Pinguenet <omegaphi.mail@gmail.com>
2019-01-23 21:52:11 +0100

    visual stuudio is hard

--- ShortcutPy for Rainmeter.py
+++ ShortcutPy for Rainmeter.py
@@ -3,6 +3,57 @@ import os, sys, winsound
 import tkinter as tk
 from tkinter import filedialog
 
+#####
+fenetre_principale = tk.Tk()
+
+####
+Widget_label_name = 0
+Widget_label_image = 0
+Widget_label_shorcut = 0
+
+Widget_label_feedback = 0
+#Widget_label_percent
+
+###
+
+Widget_entry_name = 0
+Widget_entry_image = 0
+Widget_entry_shorcut = 0
+
+###
+
+Widget_button_image = 0
+Widget_button_shortcut = 0
+Widget_button_start = 0
+
+Widget_button_image = 2
+
+if gerard == "jean":
+    lol()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #####functions
 
 def newdirectory(newdoss):
@@ -13,69 +64,66 @@ def ask_file(init_dir = "//"):
     a = filedialog.askopenfilename(initialdir = init_dir,title = "Select file")
     return a
 
-ask_file()
-
 # function that create the new shortcurt
-def new_shortcut():
-    fen = tk.Tk()
-    current_location = str(os.getcwd()) + "\\"
+# def new_shortcut():
+#     current_location = str(os.getcwd()) + "\\"
 
-    # asking variables
-    name_of_shortcut = input("Name of the Shortcut")
-    name_of_shortcut_file = name_of_shortcut
-    if os.path.exists(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".ini"):
-        file_conflit = int(input("Warning: The shortcut already exist, Do you want to replace it (1) or create a variant ? (2)"))
-        if file_conflit == "1":
-            file_conflit = "replace"
-        elif file_conflit == "2":
-            i = 0
-            while os.path.exists(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".ini"):
-                i = i + 1
-                name_of_shortcut_file = name_of_shortcut_file + " alt " + str(i)
+#     # asking variables
+#     name_of_shortcut = input("Name of the Shortcut")
+#     name_of_shortcut_file = name_of_shortcut
+#     if os.path.exists(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".ini"):
+#         file_conflit = int(input("Warning: The shortcut already exist, Do you want to replace it (1) or create a variant ? (2)"))
+#         if file_conflit == "1":
+#             file_conflit = "replace"
+#         elif file_conflit == "2":
+#             i = 0
+#             while os.path.exists(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".ini"):
+#                 i = i + 1
+#                 name_of_shortcut_file = name_of_shortcut_file + " alt " + str(i)
 
 
-    image_name = input("Image Name ?")##    debug patacorn by justpatacorn.jpg
-    image_name_file = "#@#Images\\" + image_name
-    if not os.path.exists("@Resources\\Images\\" + image_name):
-        return "The name of the image is incorect. Please try again"
+#     image_name = input("Image Name ?")##    debug patacorn by justpatacorn.jpg
+#     image_name_file = "#@#Images\\" + image_name
+#     if not os.path.exists("@Resources\\Images\\" + image_name):
+#         return "The name of the image is incorect. Please try again"
 
-    path_to_executable = input("Path to the executable")##        C:\\Users\\François\\Documents\\speak 2 me.vbs
-    if not os.path.exists(path_to_executable):
-        return "The path to the executable is incorect. Please try again"
+#     path_to_executable = input("Path to the executable")##        C:\\Users\\François\\Documents\\speak 2 me.vbs
+#     if not os.path.exists(path_to_executable):
+#         return "The path to the executable is incorect. Please try again"
 
-    #create file
-    newdirectory(current_location + "ini files\\" + name_of_shortcut + "\\")
-    with open(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".txt", mode = "w") as fichier:
-        fichier.write("[" + name_of_shortcut_file + "]" + "\n")
-        fichier.write("Meter=Image" + "\n")#############################################
-        fichier.write("ImageName=" + image_name_file + "\n")
-        fichier.write("H=90" + "\n")####################################################
-        fichier.write('LeftMouseUpAction=["' + path_to_executable +'"]' + "\n")
-        fichier.write("" + "\n")########################################################
-        fichier.write("[Rainmeter]" + "\n")#############################################
-        fichier.write("Update=1000" + "\n")#############################################
-        fichier.write("" + "\n")########################################################
-        fichier.write("[Metadata]" + "\n")##############################################
-        fichier.write("Name=" + name_of_shortcut_file + "\n")
-        fichier.write("Author=" + "\n")#################################################
-        fichier.write("Information=" + "\n")############################################
-        fichier.write("License=" + "\n")################################################
-        fichier.write("Version=" + "\n")################################################
+#     #create file
+#     newdirectory(current_location + "ini files\\" + name_of_shortcut + "\\")
+#     with open(current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".txt", mode = "w") as fichier:
+#         fichier.write("[" + name_of_shortcut_file + "]" + "\n")
+#         fichier.write("Meter=Image" + "\n")#############################################
+#         fichier.write("ImageName=" + image_name_file + "\n")
+#         fichier.write("H=90" + "\n")####################################################
+#         fichier.write('LeftMouseUpAction=["' + path_to_executable +'"]' + "\n")
+#         fichier.write("" + "\n")########################################################
+#         fichier.write("[Rainmeter]" + "\n")#############################################
+#         fichier.write("Update=1000" + "\n")#############################################
+#         fichier.write("" + "\n")########################################################
+#         fichier.write("[Metadata]" + "\n")##############################################
+#         fichier.write("Name=" + name_of_shortcut_file + "\n")
+#         fichier.write("Author=" + "\n")#################################################
+#         fichier.write("Information=" + "\n")############################################
+#         fichier.write("License=" + "\n")################################################
+#         fichier.write("Version=" + "\n")################################################
 
-    thisFile = current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".txt"
-    base = os.path.splitext(thisFile)[0]
-    os.rename(thisFile, base + ".ini")
+#     thisFile = current_location + "ini files\\" + name_of_shortcut + "\\" + name_of_shortcut_file + ".txt"
+#     base = os.path.splitext(thisFile)[0]
+#     os.rename(thisFile, base + ".ini")
 
-    print("file created")
-    return "success"
+#     print("file created")
+#     return "success"
 
 
 #mainloop
-while 1:
-    a = input("Create a new shortcut ? Y/N")
-    if a == "Y" or a == "y":
-        b = new_shortcut()
-        if b != "success":
-            print("Error: " + b)
-    else:
-        break
+#while 1:
+#    a = input("Create a new shortcut ? Y/N")
+#    if a == "Y" or a == "y":
+#        b = new_shortcut()
+#        if b != "success":
+#            print("Error: " + b)
+#    else:
+#        break
